f91a227dc53f9e86af456aec24d293991646e922
IljaDR <38908439+IljaDR@users.noreply.github.com>
2020-07-10 01:42:43 +0200

    Fixed denomination table creation, finished population function

--- database.py
+++ database.py
@@ -53,17 +53,9 @@ class Database:
                         FOREIGN KEY (EntityNumber) REFERENCES enterprise(EnterpriseNumber)
                         ); ''')
 
-        # Code
-        c.execute(''' CREATE TABLE IF NOT EXISTS code (
-                        Category varchar(16) NOT NULL,
-                        Code varchar(16) NOT NULL,
-                        Language varchar(16) NOT NULL,
-                        Description varchar(1024) NOT NULL,
-                        PRIMARY KEY (Category)
-                        ); ''')
-
         # Address
         c.execute(''' CREATE TABLE IF NOT EXISTS address (
+                        AddressID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                         EntityNumber varchar(16) NOT NULL,
                         TypeOfAddress varchar(8) NOT NULL,
                         CountryNL varchar(64),
@@ -77,39 +69,47 @@ class Database:
                         Box varchar(8),
                         ExtraAddressInfo varchar(8),
                         DateStrikingOff varchar(8),
-                        PRIMARY KEY (EntityNumber),
+                        FOREIGN KEY (EntityNumber) REFERENCES enterprise(EnterpriseNumber)
+                        ); ''')
+
+        # Code
+        c.execute(''' CREATE TABLE IF NOT EXISTS code (
+                        CodeID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        Category varchar(16) NOT NULL,
+                        Code varchar(16) NOT NULL,
+                        Language varchar(16) NOT NULL,
+                        Description varchar(1024) NOT NULL
+                        ); ''')
+
+        # Contact
+        c.execute(''' CREATE TABLE IF NOT EXISTS contact (
+                        ContactID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        EntityNumber varchar(16) NOT NULL,
+                        EntityContact varchar(8) NOT NULL,
+                        ContactType varchar(8) NOT NULL,
+                        Value varchar(64) NOT NULL,
+                        FOREIGN KEY (EntityNumber) REFERENCES enterprise(EnterpriseNumber)
+                        ); ''')
+
+        # Denomination
+        c.execute(''' CREATE TABLE IF NOT EXISTS denomination (
+                        DenominationID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        EntityNumber varchar(16) NOT NULL,
+                        Language varchar(8) NOT NULL,
+                        TypeOfDenomination varchar(8) NOT NULL,
+                        Denomination varchar(512) NOT NULL,
                         FOREIGN KEY (EntityNumber) REFERENCES enterprise(EnterpriseNumber)
                         ); ''')
 
         # Establishment
         c.execute(''' CREATE TABLE IF NOT EXISTS establishment (
+                        EstablishmentID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                         EstablishmentNumber varchar(16) NOT NULL,
-                        StartDate Date NOT NULL,
+                        StartDate Date,
                         EnterpriseNumber varchar(16) NOT NULL,
-                        PRIMARY KEY (EnterpriseNumber),
                         FOREIGN KEY (EnterpriseNumber) REFERENCES enterprise(EnterpriseNumber)
                         ); ''')
 
-        # Denomination
-        c.execute(''' CREATE TABLE IF NOT EXISTS denomination (
-                        EntityNumber varchar(16) NOT NULL,
-                        ActivityGroup varchar(8) NOT NULL,
-                        NaceVersion varchar(8) NOT NULL,
-                        NaceCode varchar(8) NOT NULL,
-                        Classification varchar(4),
-                        PRIMARY KEY (EntityNumber),
-                        FOREIGN KEY (EntityNumber) REFERENCES enterprise(EnterpriseNumber)
-                        ); ''')
-
-        # Contact
-        c.execute(''' CREATE TABLE IF NOT EXISTS contact (
-                                EntityNumber varchar(16) NOT NULL,
-                                EntityContact varchar(8) NOT NULL,
-                                ContactType varchar(8) NOT NULL,
-                                Value varchar(64) NOT NULL,
-                                PRIMARY KEY (EntityNumber),
-                                FOREIGN KEY (EntityNumber) REFERENCES enterprise(EnterpriseNumber)
-                                ); ''')
         print("Created tables if they didn't exist")
 
         conn.commit()
@@ -128,7 +128,7 @@ class Database:
         c.execute('SELECT COUNT(*) FROM enterprise')
         result = c.fetchone()
         if not result[0]:
-            with open("data/enterprise.csv") as csv_file:
+            with open("data/enterprise.csv", encoding="utf-8") as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=",")
                 line_count = 0
                 data = []
@@ -147,13 +147,14 @@ class Database:
                         c.executemany('INSERT INTO enterprise VALUES (?,?,?,?,?,?)', data)
                         del data[:]
                     line_count += 1
+            conn.commit()
             print("Populated enterprise table")
 
         # Activity
         c.execute('SELECT COUNT(*) FROM activity')
         result = c.fetchone()
         if not result[0]:
-            with open("data/activity.csv") as csv_file:
+            with open("data/activity.csv", encoding="utf-8") as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=",")
                 line_count = 0
                 data = []
@@ -167,12 +168,144 @@ class Database:
                         data += [(EntityNumber, ActivityGroup, NaceVersion, NaceCode, Classification)]
 
                     # Inserts records in batches of 100
-                    if not line_count % 1 and line_count:
-                        c.executemany('INSERT INTO activity(EntityNumber, ActivityGroup, NaceVersion, NaceCode, Classification) VALUES (?,?,?,?,?)', data)
-                        #conn.commit()
+                    if not line_count % 100 and line_count:
+                        c.executemany('INSERT INTO activity(EntityNumber, ActivityGroup, NaceVersion, '
+                                      'NaceCode, Classification) VALUES (?,?,?,?,?)', data)
                         del data[:]
                     line_count += 1
+            conn.commit()
             print("Populated activity table")
+
+        # Address
+        c.execute('SELECT COUNT(*) FROM address')
+        result = c.fetchone()
+        if not result[0]:
+            with open("data/address.csv", encoding="utf-8") as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=",")
+                line_count = 0
+                data = []
+                for row in csv_reader:
+                    if line_count:
+                        EntityNumber = row[0]
+                        TypeOfAddress = row[1]
+                        CountryNL = row[2]
+                        CountryFR = row[3]
+                        Zipcode = row[4]
+                        MunicipalityNL = row[5]
+                        MunicipalityFR = row[6]
+                        StreetNL = row[7]
+                        StreetFR = row[8]
+                        HouseNumber = row[9]
+                        Box = row[10]
+                        ExtraAddressInfo = row[11]
+                        DateStrikingOff = row[12]
+                        data += [(EntityNumber, TypeOfAddress, CountryNL, CountryFR, Zipcode,
+                                  MunicipalityNL, MunicipalityFR, StreetNL, StreetFR, HouseNumber,
+                                  Box, ExtraAddressInfo, DateStrikingOff)]
+                    # Inserts records in batches of 100
+                    if not line_count % 100 and line_count:
+                        c.executemany('INSERT INTO address(EntityNumber, TypeOfAddress, CountryNL, CountryFR, Zipcode,\
+                                  MunicipalityNL, MunicipalityFR, StreetNL, StreetFR, HouseNumber,\
+                                  Box, ExtraAddressInfo, DateStrikingOff) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)', data)
+                        del data[:]
+                    line_count += 1
+            conn.commit()
+            print("Populated address table")
+
+        # Code
+        c.execute('SELECT COUNT(*) FROM code')
+        result = c.fetchone()
+        if not result[0]:
+            with open("data/Code.csv", encoding="utf-8") as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=",")
+                line_count = 0
+                data = []
+                for row in csv_reader:
+                    if line_count:
+                        Category = row[0]
+                        Code = row[1]
+                        Language = row[2]
+                        Description = row[3]
+                        data += [(Category, Code, Language, Description)]
+                    # Inserts records in batches of 100
+                    if not line_count % 100 and line_count:
+                        c.executemany('INSERT INTO code(Category, Code, Language, Description) VALUES (?,?,?,?)', data)
+                        del data[:]
+                    line_count += 1
+            conn.commit()
+            print("Populated code table")
+
+        # Contact
+        c.execute('SELECT COUNT(*) FROM contact')
+        result = c.fetchone()
+        if not result[0]:
+            with open("data/Contact.csv", encoding="utf-8") as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=",")
+                line_count = 0
+                data = []
+                for row in csv_reader:
+                    if line_count:
+                        EntityNumber = row[0]
+                        EntityContact = row[1]
+                        ContactType = row[2]
+                        Value = row[3]
+                        data += [(EntityNumber, EntityContact, ContactType, Value)]
+                    # Inserts records in batches of 100
+                    if not line_count % 100 and line_count:
+                        c.executemany('INSERT INTO contact(EntityNumber, EntityContact, ContactType, Value) VALUES (?,?,?,?)',
+                                      data)
+                        del data[:]
+                    line_count += 1
+            conn.commit()
+            print("Populated contact table")
+
+        # Denomination
+        c.execute('SELECT COUNT(*) FROM denomination')
+        result = c.fetchone()
+        if not result[0]:
+            with open("data/Denomination.csv", encoding="utf-8") as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=",")
+                line_count = 0
+                data = []
+                for row in csv_reader:
+                    if line_count:
+                        EntityNumber = row[0]
+                        Language = row[1]
+                        TypeOfDenomination = row[2]
+                        Denomination = row[3]
+                        data += [(EntityNumber, Language, TypeOfDenomination, Denomination)]
+                    # Inserts records in batches of 100
+                    if not line_count % 100 and line_count:
+                        c.executemany('INSERT INTO denomination(EntityNumber, Language, TypeOfDenomination, Denomination) VALUES (?,?,?,?)',
+                            data)
+                        del data[:]
+                    line_count += 1
+            conn.commit()
+            print("Populated denomination table")
+
+        # Establishment
+        c.execute('SELECT COUNT(*) FROM establishment')
+        result = c.fetchone()
+        if not result[0]:
+            with open("data/Establishment.csv", encoding="utf-8") as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=",")
+                line_count = 0
+                data = []
+                for row in csv_reader:
+                    if line_count:
+                        EstablishmentNumber = row[0]
+                        StartDate = row[1]
+                        EnterpriseNumber = row[2]
+                        data += [(EstablishmentNumber, StartDate, EnterpriseNumber)]
+                    # Inserts records in batches of 100
+                    if not line_count % 100 and line_count:
+                        c.executemany(
+                            'INSERT INTO establishment(EstablishmentNumber, EnterpriseNumber, EnterpriseNumber) VALUES (?,?,?)',
+                            data)
+                        del data[:]
+                    line_count += 1
+            conn.commit()
+            print("Populated establishment table")
 
         conn.commit()
         conn.close()
